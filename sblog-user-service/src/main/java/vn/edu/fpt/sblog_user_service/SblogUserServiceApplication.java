f512f303879592c582fd397de112896040a30004
Huy Nguyen <huyhol287@gmail.com>
2025-08-14 14:56:29 +0700

    create user service

---
+++ sblog-user-service/src/main/java/vn/edu/fpt/sblog_user_service/SblogUserServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblogUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblogUserServiceApplication.class, args);
+	}
+
+}
---
+++ sblog-user-service/src/test/java/vn/edu/fpt/sblog_user_service/SblogUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SblogUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
