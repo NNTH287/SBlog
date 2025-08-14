9c8e8873771f549068b316a96830a8dcfb04a924
Huy Nguyen <huyhol287@gmail.com>
2025-08-14 13:42:51 +0700

    create config server

---
+++ sblog-config-server/src/main/java/vn/edu/fpt/sblog_config_server/SblogConfigServerApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblogConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblogConfigServerApplication.class, args);
+	}
+
+}
---
+++ sblog-config-server/src/test/java/vn/edu/fpt/sblog_config_server/SblogConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SblogConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
