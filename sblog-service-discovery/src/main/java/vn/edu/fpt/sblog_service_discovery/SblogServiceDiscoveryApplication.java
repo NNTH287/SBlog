9f192369c208f5aba1b284df30b5932b5343ac02
Huy Nguyen <huyhol287@gmail.com>
2025-08-14 14:16:49 +0700

    create service discovery

---
+++ sblog-service-discovery/src/main/java/vn/edu/fpt/sblog_service_discovery/SblogServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_service_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblogServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblogServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ sblog-service-discovery/src/test/java/vn/edu/fpt/sblog_service_discovery/SblogServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SblogServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
