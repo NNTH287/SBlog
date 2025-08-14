cff2ff8280404dbb385245983df278b606485adf
Huy Nguyen <huyhol287@gmail.com>
2025-08-14 14:34:01 +0700

    create API gateway

---
+++ sblog-api-gateway/src/main/java/vn/edu/fpt/sblog_api_gateway/SblogApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblogApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblogApiGatewayApplication.class, args);
+	}
+
+}
