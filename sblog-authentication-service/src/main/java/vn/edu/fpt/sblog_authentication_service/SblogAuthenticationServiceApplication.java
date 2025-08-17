1d9d2cf3241c427d364432597727c5ea51a80354
Huy Nguyen <huyhol287@gmail.com>
2025-08-17 17:57:43 +0700

    creat authentication service

---
+++ sblog-authentication-service/src/main/java/vn/edu/fpt/sblog_authentication_service/SblogAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_authentication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblogAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblogAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ sblog-authentication-service/src/test/java/vn/edu/fpt/sblog_authentication_service/SblogAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SblogAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
