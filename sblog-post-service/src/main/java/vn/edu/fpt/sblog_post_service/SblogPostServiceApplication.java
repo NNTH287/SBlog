a17893c4c9d20c4c8ad5f579bef9c660d3448677
Huy Nguyen <huyhol287@gmail.com>
2025-08-20 03:25:52 +0700

    create post service

---
+++ sblog-post-service/src/main/java/vn/edu/fpt/sblog_post_service/SblogPostServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_post_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblogPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblogPostServiceApplication.class, args);
+	}
+
+}
---
+++ sblog-post-service/src/test/java/vn/edu/fpt/sblog_post_service/SblogPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.sblog_post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SblogPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
